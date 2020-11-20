38383a7309b5e016026918e9e8390ade23604302
draftpick <draft-pick@yandex.ru>
2020-11-20 16:12:22 +0300

    MACBOOK

--- main/views.py
+++ main/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import *
+from news.models import News, GalleryNews
 
 
 def index(request):
+    news = News.objects.order_by("-id")[0:4]
+    image = GalleryNews.objects.filter(in_the_title=1)
     context = {
+        'news': news,
+        'image': image,
         'title': 'Главная страница',
     }
     return render(request, 'main/index.html', context=context)
