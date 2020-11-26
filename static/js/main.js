6045fb7745144a86a82273e2dcc1edfef3ea2e58
draftpick <draft-pick@yandex.ru>
2020-11-26 16:00:46 +0300

    MACBOOK

--- static/js/main.js
+++ static/js/main.js
@@ -66,6 +66,29 @@ $(document).ready(function() {
             });
         });
 
+$(document).ready(function() {
+            $('#carouselNews').owlCarousel({
+                loop:true, //Зацикливаем слайдер
+                margin:50, //Отступ от элемента справа в 50px
+                nav:true, //Отключение навигации
+                autoplay:true, //Автозапуск слайдера
+                autoplayHoverPause:true,
+                smartSpeed:2000, //Время движения слайда
+                autoplayTimeout:5000, //Время смены слайда
+                responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:2
+                    },
+                    1000:{
+                        items:4
+                    }
+                }
+            });
+        });
+
 (function($) {
   $.fn.truncate = function(options) {
 
